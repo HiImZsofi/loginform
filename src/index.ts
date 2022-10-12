39a50fdae225ea14db0d3fb7d323106f2e9869e7
HiImZsofi <zsofia.gyori00@gmail.com>
2022-10-12 14:14:28 +0200

    check email addresses

--- src/index.ts
+++ src/index.ts
@@ -15,4 +15,21 @@ document.addEventListener("DOMContentLoaded", () => {
     else{
             (document.getElementById('usererror') as HTMLElement).textContent = "A felhasználónév nem lehet üres!"
     }
+
+    function validateEmail(email: string) {
+        var re = /\S+@\S+\.\S+/;
+        return re.test(email);
+      }
+
+        const emailinput = document.getElementById('email') as HTMLInputElement;
+        const emailvalue = emailinput.value;    
+        const emailinput2 = document.getElementById('email2') as HTMLInputElement;
+        const emailvalue2 = emailinput2.value;
+
+      if(validateEmail(emailvalue) == false){
+        (document.getElementById('emailerror') as HTMLElement).textContent = "Az email cím nem felel meg a követelményeknek"
+      }
+      else if(emailvalue !== emailvalue2){
+        (document.getElementById('emailerror') as HTMLElement).textContent = "A megadott email címek nem egyeznek egymással"
+      }
 })
